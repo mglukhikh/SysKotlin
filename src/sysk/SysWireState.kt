109e3072766b08bf6e60144d6755ffa58de31f34
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-09-18 10:39:57 +0300

    SysWireState (tri-state wire) introduced

---
+++ src/sysk/SysWireState.kt
@@ -0,0 +1,28 @@
+package sysk
+
+enum class SysWireState {
+    ONE {
+        override val one = true
+    },
+    ZERO {
+        override val zero = true
+    },
+    X {
+        override val x = true
+    };
+
+    open val one = false
+    open val zero = false
+    open val x = false
+
+    fun not() = when (this) {
+        X    -> X
+        ONE  -> ZERO
+        ZERO -> ONE
+    }
+
+    fun and(other: SysWireState) = if (this == ZERO || other == ZERO) ZERO else if (this == ONE && other == ONE) ONE else X
+
+    fun or(other: SysWireState) = !((!other).and(!this))
+
+}
---
+++ test/sysk/SysWireStateTest.kt
@@ -0,0 +1,47 @@
+package sysk
+
+import org.junit.Test
+
+public class SysWireStateTest {
+
+    @Test
+    fun base() {
+        assert(SysWireState.ZERO.zero)
+        assert(SysWireState.ONE.one)
+        assert(SysWireState.X.x)
+    }
+
+    @Test
+    fun not() {
+        assert((!SysWireState.ZERO).one)
+        assert((!SysWireState.ONE).zero)
+        assert((!SysWireState.X).x)
+    }
+
+    @Test
+    fun or() {
+        for (arg in SysWireState.values()) {
+            assert(SysWireState.ONE.or(arg).one)
+            assert(arg.or(SysWireState.ONE).one)
+            if (!arg.one) {
+                assert(SysWireState.X.or(arg).x)
+                assert(arg.or(SysWireState.X).x)
+            }
+        }
+        assert(SysWireState.ZERO.or(SysWireState.ZERO).zero)
+    }
+
+
+    @Test
+    fun and() {
+        for (arg in SysWireState.values()) {
+            assert(SysWireState.ZERO.and(arg).zero)
+            assert(arg.and(SysWireState.ZERO).zero)
+            if (!arg.zero) {
+                assert(SysWireState.X.and(arg).x)
+                assert(arg.and(SysWireState.X).x)
+            }
+        }
+        assert(SysWireState.ONE.and(SysWireState.ONE).one)
+    }
+}
