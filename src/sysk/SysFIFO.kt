4ba0e76f2c382ff145720089efa653546b549894
Mikhail Glukhikh <mikhail.glukhikh@gmail.com>
2015-10-08 22:09:52 +0300

    FIFO minor refactoring

--- src/sysk/SysFIFO.kt
+++ src/sysk/SysFIFO.kt
@@ -4,52 +4,54 @@ import java.util.*
 open class SysFIFO<T>(open val capacity: Int, name: String, startValue: T, scheduler: SysScheduler, parent: SysObject? = null):
         SysSignalRead<T>, SysObject(name, parent) {
 
-    private val changeEvent = SysWait.Event("changeEvent", scheduler, this);
+    private val changeEvent = SysWait.Event("changeEvent", scheduler, this)
 
-    protected var fifo: Queue<T> = LinkedList();
+    protected var fifo: Queue<T> = LinkedList()
 
     /** Head of queue */
-    override var value = startValue; // Excess
-        private set;
+    override var value = startValue // Excess
+        private set
 
     /** Interface*/
-    public var input: T = startValue
-        set(value) = push(value); // How make a private get?
+    // TODO: rename to output?
+    var input: T
+        get() = throw UnsupportedOperationException("Input property is write-only for FIFO")
+        set(value) = push(value)
 
-    public val output: T
-        get() = value;
+    // TODO: rename to input?
+    val output: T
+        get() = value
 
-    public val size: Int
-        get() = fifo.size;
+    val size: Int
+        get() = fifo.size
 
-    public val empty: Boolean
-        get() = (fifo.isEmpty());
+    val empty: Boolean
+        get() = fifo.isEmpty()
 
-    public val full: Boolean
-        get() = (fifo.size == capacity);
+    val full: Boolean
+        get() = fifo.size == capacity
 
     open fun pop() { // How to convert in the class field?
-        if (!fifo.isEmpty()) fifo.remove();
-        if (!fifo.isEmpty()
-                && (value != fifo.element())) {
-            value = fifo.element();
-            changeEvent.happens();
+        if (!fifo.isEmpty()) fifo.remove()
+        if (!fifo.isEmpty() && value != fifo.element()) {
+            value = fifo.element()
+            changeEvent.happens()
         }
     }
 
     override val defaultEvent: SysWait.Event
-        get() = changeEvent;
+        get() = changeEvent
 
-    public fun push(value: T) {
-        if (!full) fifo.add(value);
-        this.value = fifo.element();
+    fun push(value: T) {
+        if (!full) fifo.add(value)
+        this.value = fifo.element()
     }
 
-    override fun read() = value; // Excess
+    override fun read() = value // Excess
 
     override fun register(port: SysPort<*>) { }
 
-    override fun toString() = fifo.toString();
+    override fun toString() = fifo.toString()
 }
 
 open class SysWireFIFO(capacity: Int, name: String, scheduler: SysScheduler, parent: SysObject? = null)
@@ -60,13 +62,13 @@ open class SysWireFIFO(capacity: Int, name: String, scheduler: SysScheduler, par
     override val negEdgeEvent = SysWait.Event("negEdgeEvent", scheduler, this)
 
     val zero: Boolean
-        get() = value.zero;
+        get() = value.zero
 
     val one: Boolean
-        get() = value.one;
+        get() = value.one
 
     val x: Boolean
-        get() = value.x;
+        get() = value.x
 
     override fun pop() {
         val prevValue = this.value;
