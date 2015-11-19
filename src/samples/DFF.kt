a070e2a0ad83bf350aa4273ddca20423a8785351
Mikhail Glukhikh <mikhail.glukhikh@gmail.com>
2015-11-19 18:00:28 +0300

    A pair of trigger TODOs added

--- src/samples/DFF.kt
+++ src/samples/DFF.kt
@@ -12,6 +12,8 @@ public class DFF (name: String, parent: SysModule): SysModule(name, parent) {
 
     init {
         triggeredFunction(clk, initialize = false) {
+            // TODO [glukhikh]: think why we have two clock ticks delay even with q.value = state after state change
+            // TODO [glukhikh]: q.value = state should be ahead: here and in all other FFs
             if (d.one) state = SysWireState.ONE
             else if (d.zero) state = SysWireState.ZERO
             q.value = state
