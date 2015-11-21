0995b2b1c2e1d2287dc7d37b8d091f90dca62e10
Mikhail Glukhikh <mikhail.glukhikh@gmail.com>
2015-11-22 01:02:04 +0300

    Universal register: rename, + enable input, TODO

---
+++ src/samples/Register.kt
@@ -0,0 +1,29 @@
+package samples
+
+import sysk.*
+
+/**
+ * Register for storing any kind of information
+ */
+public class Register <T> (name: String, defValue: T, parent: SysModule): SysModule(name, parent) {
+
+    val d = input<T>("d")      // data input
+    val en = wireInput("en")   // enable
+    val clk = wireInput("clk") // clock
+
+    private var state: T = defValue
+    val q = output<T>("q")     // data output
+
+    init {
+        triggeredFunction(clk, initialize = false) {
+            // TODO [veronika]: tests
+            if (en.one) {
+                q.value = d.value
+                state = d.value
+            }
+            else {
+                q.value = state
+            }
+        }
+    }
+}
