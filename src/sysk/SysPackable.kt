a56ca3abd726e7d6809a1cef6928a0e16e737742
Mikhail Glukhikh <mikhail.glukhikh@gmail.com>
2015-12-03 14:46:58 +0300

    Minor

--- src/sysk/SysPackable.kt
+++ src/sysk/SysPackable.kt
@@ -5,8 +5,6 @@ interface SysPackable {
     val undefined: Boolean
         get() = false
 
-    override fun toString(): String
-
     companion object Undefined : SysPackable {
         override val undefined: Boolean
             get() = true
