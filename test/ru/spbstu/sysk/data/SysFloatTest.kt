cf44636d615b599280e38961ad9662e74e9e3944
Saloed <napster_1997@mail.ru>
2016-03-06 21:23:13 +0300

    Test for SysFloat
    Only math

--- test/ru/spbstu/sysk/data/SysFloatTest.kt
+++ test/ru/spbstu/sysk/data/SysFloatTest.kt
@@ -7,28 +7,53 @@ class SysFloatTest {
     @Test
     fun constructionTest() {
 
-        val x: Double = 12.34567
         val y: Float = 12.34567f
-        val a = SysFloat.valueOf(x)
-        val b = SysFloat.valueOf(127.55555)
         val c = SysFloat.valueOf(y)
-        assert(x.toFloat() == y)
-
-        assert(y.toDouble() != x)
-
-        assert(a.toDouble() == x)
         assert(c.toFloat() == y)
-        assert(b.toDouble() == 127.55555)
+
     }
 
     @Test
     fun mathTest() {
 
-        val a = SysFloat.valueOf(0.253)
-        val b = SysFloat.valueOf(100.7)
-        var c = SysFloat.valueOf(100.953)
-        assert(a + b == c)
+        val af = 0.5f
+        val bf = 10.0f
+        val cf = 10.5f
 
+        val a = SysFloat.valueOf(af)
+        val b = SysFloat.valueOf(bf)
+        val c = SysFloat.valueOf(cf)
+
+        assert(af + bf == cf) { af + bf }
+        assert(a + b == c) { "plus" }
+        assert(cf - bf == af) { cf - bf }
+        assert(c - b == a) { "minus" }
+        assert(cf - af == bf) { cf - af }
+        assert(c - a == b) { "minus" }
+
+        val xf = 23.55f
+        val yf = 1.2f
+        val zf = 28.26f
+
+        val x = SysFloat.valueOf(xf)
+        val y = SysFloat.valueOf(yf)
+        val z = SysFloat.valueOf(zf)
+
+        assert(xf * yf == zf) { xf * yf }
+        assert(x * y == z) { "mult" }
+        assert(zf / yf == xf) { zf / yf }
+        assert(z / y == x) { "div" }
+
+        val ef = 2.0f
+        val nf = 16.16f
+        val mf = 261.1456f
+
+        val e = SysFloat.valueOf(ef)
+        val n = SysFloat.valueOf(nf)
+        val m = SysFloat.valueOf(mf)
+
+        assert(Math.pow(nf.toDouble(), ef.toDouble()).toFloat() == mf)
+        assert(n.power(e) == m) { "power" }
     }
 
 }
