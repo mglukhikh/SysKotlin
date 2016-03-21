6c7238b9354c3a15387578db25531fa2ba8ee87e
Saloed <napster_1997@mail.ru>
2016-03-22 01:15:08 +0300

    Just simple test for IntegerBase

---
+++ src/test/java/ru/spbstu/sysk/data/SysBaseIntegerTest.kt
@@ -0,0 +1,22 @@
+package ru.spbstu.sysk.data
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class SysBaseIntegerTest {
+
+
+    @Test
+    fun test() {
+
+        val a = SysBaseInteger.valueOf(32, 2222222)
+        val b = SysBaseInteger.valueOf(100, 2222222)
+        assertEquals(b + a, SysBigInteger(100, 4444444))
+        assertNotEquals(a + b, SysBigInteger(100, 4444444))
+        assertEquals(a + b, SysInteger(SysInteger.MAX_WIDTH, 4444444))
+
+
+    }
+
+}
