64b66e2ab37420282f9f0cc4c8b293cc2df7b8a4
Sergei <seryoga@computercraft.ru>
2016-04-18 21:02:05 +0300

    Simplify

--- src/main/java/ru/spbstu/sysk/samples/triggers/nRS.kt
+++ src/main/java/ru/spbstu/sysk/samples/triggers/nRS.kt
@@ -14,24 +14,18 @@ class nRS(name: String, parent: SysModule) : SysModule(name, parent) {
     private val u1 = NAND("u1", this)
     private val d1 = NAND("d1", this)
 
-    private val nr = bitSignal("nr")
-    private val ns = bitSignal("ns")
     private val q = bitSignal("q")
     private val nq = bitSignal("nq")
 
     init {
-        u1.x1 bind ns
+        u1.x1 bind nS
         u1.x2 bind nq
         u1.y bind q
 
         d1.x1 bind q
-        d1.x2 bind nr
+        d1.x2 bind nR
         d1.y bind nq
 
-        function(nR.defaultEvent or nS.defaultEvent) {
-            nr(nR())
-            ns(nS())
-        }
         function(q.defaultEvent or nq.defaultEvent) {
             Q(q())
             nQ(nq())
